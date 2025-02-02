b185f4b7a301832449fe1f7fddca9d66ac4f1d64
Zeel <pzeel881@gmail.com>
2025-02-01 19:37:22 -0500

    Updated ExploreItems

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -4,18 +4,17 @@ import { Link } from "react-router-dom";
 const ExploreItems = () => {
   const [items, setItems] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     const fetchItems = async () => {
       try {
-        const response = await fetch("https://us-central1-nft-cloud-functions.cloudfunctions.net/explore");
+        const response = await fetch(`https://us-central1-nft-cloud-functions.cloudfunctions.net/explore?page=${page}`);
         const data = await response.json();
-        setItems(
-          data.map((item) => ({
-            ...item,
-            timer: item.timer - Math.floor(Date.now() / 1000),
-          }))
-        );
+        setItems((prevItems) => [...prevItems, ...data.map((item) => ({
+          ...item,
+          timer: item.timer - Math.floor(Date.now() / 1000),
+        }))]);
       } catch (error) {
         console.error("Error fetching explore items:", error);
       } finally {
@@ -24,7 +23,7 @@ const ExploreItems = () => {
     };
 
     fetchItems();
-  }, []);
+  }, [page]);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -46,25 +45,9 @@ const ExploreItems = () => {
     return `${hrs}:${mins}:${secs}`;
   };
 
-  const SkeletonItem = () => (
-    <div
-      className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12 skeleton-item"
-      style={{ display: "block" }}
-    >
-      <div className="nft__item skeleton">
-        <div className="author_list_pp">
-          <div className="skeleton-box" style={{ width: 50, height: 50, borderRadius: "50%" }}></div>
-        </div>
-        <div className="nft__item_wrap">
-          <div className="skeleton-box" style={{ width: "100%", height: 150 }}></div>
-        </div>
-        <div className="nft__item_info">
-          <div className="skeleton-box" style={{ width: "60%", height: 20, marginBottom: 10 }}></div>
-          <div className="skeleton-box" style={{ width: "40%", height: 20 }}></div>
-        </div>
-      </div>
-    </div>
-  );
+  const handleLoadMore = () => {
+    setPage((prevPage) => prevPage + 1);
+  };
 
   return (
     <>
@@ -77,78 +60,49 @@ const ExploreItems = () => {
         </select>
       </div>
       <div className="row">
-        {loading
-          ? new Array(8).fill(0).map((_, index) => <SkeletonItem key={index} />)
+        {loading && items.length === 0
+          ? new Array(8).fill(0).map((_, index) => <div key={index} className="skeleton-item">Loading...</div>)
           : items.map((item, index) => (
-            <div
-              key={index}
-              className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
-              style={{ display: "block" }}
-            >
-              <div className="nft__item">
-                <div className="author_list_pp">
-                  <Link
-                    to={`/author/${item.authorId}`}
-                    data-bs-toggle="tooltip"
-                    data-bs-placement="top"
-                  >
-                    <img
-                      className="lazy"
-                      src={item.authorImage}
-                      alt={item.authorName}
-                    />
-                    <i className="fa fa-check"></i>
-                  </Link>
-                </div>
-                <div className="de_countdown">{formatTime(item.timer)}</div>
-
-                <div className="nft__item_wrap">
-                  <div className="nft__item_extra">
-                    <div className="nft__item_buttons">
-                      <button>Buy Now</button>
-                      <div className="nft__item_share">
-                        <h4>Share</h4>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-facebook fa-lg"></i>
-                        </a>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-twitter fa-lg"></i>
-                        </a>
-                        <a href="">
-                          <i className="fa fa-envelope fa-lg"></i>
-                        </a>
-                      </div>
-                    </div>
+              <div
+                key={index}
+                className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
+                style={{ display: "block" }}
+              >
+                <div className="nft__item">
+                  <div className="author_list_pp">
+                    <Link to={`/author/${item.authorId}`}>
+                      <img className="lazy" src={item.authorImage} alt={item.authorName} />
+                      <i className="fa fa-check"></i>
+                    </Link>
                   </div>
-                  <Link to={`/item-details/${item.nftId}`}>
+                  <div className="de_countdown">{formatTime(item.timer)}</div>
 
-                    <img
-                      src={item.nftImage}
-                      className="lazy nft__item_preview"
-                      alt={item.title}
-                    />
-                  </Link>
+                  <div className="nft__item_wrap">
+                    <Link to={`/item-details/${item.id}`}>
+                      <img src={item.nftImage} className="lazy nft__item_preview" alt={item.title} />
+                    </Link>
+                  </div>
 
-                </div>
-                
-                <div className="nft__item_info">
-                  <Link to={`/item-details/${item.id}`}>
-                    <h4>{item.title}</h4>
-                  </Link>
-                  <div className="nft__item_price">{item.price} ETH</div>
-                  <div className="nft__item_like">
-                    <i className="fa fa-heart"></i>
-                    <span>{item.likes}</span>
+                  <div className="nft__item_info">
+                    <Link to={`/item-details/${item.id}`}>
+                      <h4>{item.title}</h4>
+                    </Link>
+                    <div className="nft__item_price">{item.price} ETH</div>
+                    <div className="nft__item_like">
+                      <i className="fa fa-heart"></i>
+                      <span>{item.likes}</span>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
       </div>
       <div className="col-md-12 text-center">
-        <Link to="" id="loadmore" className="btn-main lead">
-          Load more
-        </Link>
+        {!loading && (
+          <button id="loadmore" className="btn-main lead" onClick={handleLoadMore}>
+            Load more
+          </button>
+        )}
       </div>
     </>
   );
